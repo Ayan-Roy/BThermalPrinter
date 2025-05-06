cf788abe43fa3c832664714383e2b0e9df46a75e
Ayan Roy <ayanroycse@gmail.com>
2025-05-06 13:01:27 +0600

    Bluetooth Printing Library implemented

--- app/src/main/java/com/swosti/bthermalprinter/MainActivity.java
+++ app/src/main/java/com/swosti/bthermalprinter/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -21,6 +22,7 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     private MaterialButton btnPrint;
     public static final int PERMISSION_BLUETOOTH = 1;
 
@@ -42,9 +44,27 @@ public class MainActivity extends AppCompatActivity {
         btnPrint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkBluetoothPermissions();
+
+                if (checkBluetoothEnabled())
+                    checkBluetoothPermissions();
             }
         });
+    }
+
+    private boolean checkBluetoothEnabled() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.d(TAG, "Device does not support Bluetooth");
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!bluetoothAdapter.isEnabled()) {
+            Log.d("Bluetooth", "Bluetooth is disabled");
+            Toast.makeText(this, "PLease Turn On Bluetooth", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            Log.d("Bluetooth", "Bluetooth is enabled");
+            return true;
+        }
     }
 
 
@@ -59,9 +79,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, MainActivity.PERMISSION_BLUETOOTH_SCAN);
         } else {
-
             try {
-                Toast.makeText(this, "All Permission Granted!", Toast.LENGTH_SHORT).show();
                 printBluetooth();
             } catch (Exception exp) {
                 exp.printStackTrace();
@@ -86,36 +104,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printBluetooth() throws Exception {
-
         EscPosPrinter printer = new EscPosPrinter(BluetoothPrintersConnections.selectFirstPaired(), 203, 48f, 32);
-        printer.printFormattedText(
-                        "[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, this.getApplicationContext().getResources().getDrawableForDensity(R.drawable.icon_app_store, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
-                                "[L]\n" +
-                                "[C]<u><font size='big'>ORDER N°045</font></u>\n" +
-                                "[L]\n" +
-                                "[C]================================\n" +
-                                "[L]\n" +
-                                "[L]<b>BEAUTIFUL SHIRT</b>[R]9.99e\n" +
-                                "[L]  + Size : S\n" +
-                                "[L]\n" +
-                                "[L]<b>AWESOME HAT</b>[R]24.99e\n" +
-                                "[L]  + Size : 57/58\n" +
-                                "[L]\n" +
-                                "[C]--------------------------------\n" +
-                                "[R]TOTAL PRICE :[R]34.98e\n" +
-                                "[R]TAX :[R]4.23e\n" +
-                                "[L]\n" +
-                                "[C]================================\n" +
-                                "[L]\n" +
-                                "[L]<font size='tall'>Customer :</font>\n" +
-                                "[L]Raymond DUPONT\n" +
-                                "[L]5 rue des girafes\n" +
-                                "[L]31547 PERPETES\n" +
-                                "[L]Tel : +33801201456\n" +
-                                "[L]\n" +
-                                "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
-                                "[C]<qrcode size='20'>https://dantsu.com/</qrcode>"
-                );
+        printer.printFormattedText(getInvoiceDataAsText(printer));
+        Log.e(TAG, "Invoice Printed:");
+        Toast.makeText(this, "Invoice Printed", Toast.LENGTH_SHORT).show();
 
     }
+
+    private String getInvoiceDataAsText(EscPosPrinter printer) {
+
+        String rptData = "[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, this.getApplicationContext().getResources().getDrawableForDensity(R.drawable.icon_app_store, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
+                "[L]\n" +
+                "[C]<u><font size='big'>Demo Organization</font></u>\n";
+
+
+        return rptData;
+    }
+
+
+ /*
+"[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, this.getApplicationContext().getResources().getDrawableForDensity(R.drawable.icon_app_store, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
+            "[L]\n" +
+            "[C]<u><font size='big'>Demo Organization</font></u>\n" +
+            "[L]\n" +
+            "[C]================================\n" +
+            "[L]\n" +
+            "[L]<b>BEAUTIFUL SHIRT</b>[R]9.99e\n" +
+            "[L]  + Size : S\n" +
+            "[L]\n" +
+            "[L]<b>AWESOME HAT</b>[R]24.99e\n" +
+            "[L]  + Size : 57/58\n" +
+            "[L]\n" +
+            "[C]--------------------------------\n" +
+            "[R]TOTAL PRICE :[R]34.98e\n" +
+            "[R]TAX :[R]4.23e\n" +
+            "[L]\n" +
+            "[C]================================\n" +
+            "[L]\n" +
+            "[L]<font size='tall'>Customer :</font>\n" +
+            "[L]Raymond DUPONT\n" +
+            "[L]5 rue des girafes\n" +
+            "[L]31547 PERPETES\n" +
+            "[L]Tel : +33801201456\n" +
+            "[L]\n" +
+            "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
+            "[C]<qrcode size='20'>https://dantsu.com/</qrcode>";*/
+
+
 }
